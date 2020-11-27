949520dd85353af7aa7d39a68daddd7a0c595a80
fujc <fjc.dane@gmail.com>
2020-11-27 13:07:51 +0800

    JAVA

---
+++ src/main/java/com/zc58s/structural/facade/Program.java
@@ -0,0 +1,13 @@
+package com.zc58s.structural.facade;
+
+/*
+ * @description :
+ * 概述：
+ * 外观(门面)模式，
+ *
+ *
+ * @author      : fjc.dane@gmail.com
+ * @create time : 2020/11/27 13:06
+ */
+public class Program {
+}
