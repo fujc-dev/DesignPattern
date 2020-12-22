6aa16d760d83b107829329c48e044a10f5e9bc63
fujc <fjc.dane@gmail.com>
2020-12-22 16:05:59 +0800

    Java基础-反射

---
+++ src/main/java/com/zc58s/basic/reflection/P225.java
@@ -0,0 +1,16 @@
+package com.zc58s.basic.reflection;
+
+import com.zc58s.basic.reflection.pojo.Employee;
+
+/*
+ * DesignPattern
+ * com.zc58s.basic.reflection.readme
+ *
+ * @author      : fjc.dane@gmail.com
+ * @create time : 2020/12/22 15:02
+ */
+public class P225 {
+    public static void main(String[] args) {
+        Class c1 = Employee.class;
+    }
+}
